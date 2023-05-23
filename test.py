8b4e27fe36732f5258e4473f13b5087918f8fb74
naruhirokimura <112096162+naruhirokimura@users.noreply.github.com>
2023-05-24 08:32:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,11 @@ st.write("hellow")
 
 with open('model.pickle', 'rb') as f:
     model = pickle.load(f)
+
+Tbil = st.sidebar.slider(label='T-Bil (mg/dL)', min_value=0.2, max_value=4.3,value=1.0, step=0.1)
+TP = st.sidebar.slider(label='Total protein (g/dL)', min_value=0.2, max_value=4.3,value=1.0, step=0.1)
+ALT = st.sidebar.slider(label='ALT (IU/L)', min_value=1600, max_value=11400,value=100)
+
+sample = np.array([['Tbil','TP','ALP'],[Tbil, TP, ALT]])
+dfsample = pd.DataFrame(data=[[Tbil, TP, ALT]], columns=['T-Bil (md/dl)', 'Total protein (g/dL)', 'ALT (IU/L)'])
+st.write(dfsample)    
