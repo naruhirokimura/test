077bb3299d40655a72908f1e4062cfd02b97fa42
naruhirokimura <112096162+naruhirokimura@users.noreply.github.com>
2023-05-24 14:08:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import pickle
 import numpy as np
 import pandas as pd
 import streamlit as st
+from sklearn.model_selection import train_test_split
+from xgboost import XGBClassifier
+from sklearn import metrics
 
 # 学習データCSVファイル読み込み
 df = pd.read_csv('data for streamlit 20230524.csv')
