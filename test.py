62e81a2d3e6dcb9b32d27638e992b64d4de1a68a
naruhirokimura <112096162+naruhirokimura@users.noreply.github.com>
2023-05-24 16:26:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn import metrics
+import matplotlib.pyplot as plt
 
 # 学習データCSVファイル読み込み
 df = pd.read_csv('data for streamlit 20230524.csv')
